70c959ebd9d28fe2268c010fc54e52c5c949300f
Alexandre Ribeiro <alexandreribeiro0209@gmail.com>
2024-02-28 17:18:28 -0300

    description doc and adjust

--- verificaPrimo.c
+++ verificaPrimo.c
@@ -1,10 +1,12 @@
-// Exercício 14: Escreva uma função em C chamada verificaPrimo que receba um número inteiro como parâmetro e retorne verdadeiro se o número for primo e falso caso contrário. Em seguida, escreva um programa principal que leia um número fornecido pelo usuário e chame a função verificaPrimo para verificar se é primo ou não, imprimindo o resultado correspondente.
+// Escreva uma função em C chamada verificaPrimo que receba um número inteiro como parâmetro e retorne verdadeiro se o número for primo e falso caso contrário. Em seguida, escreva um programa principal que leia um número fornecido pelo usuário e chame a função verificaPrimo para verificar se é primo ou não, imprimindo o resultado correspondente.
 
+// Inclusão da biblioteca Standard Input Output
 #include <stdio.h>
 
 // Prototipação
 int verificaPrimo(int);
 
+// Função principal
 int main(){
 
     int num;
@@ -14,7 +16,7 @@ int main(){
 
     int verific = verificaPrimo(num);
 
-    if (verific == 1)
+    if (verific)
     {
         printf("O numero %d e primo", num);
     } else {
@@ -23,8 +25,9 @@ int main(){
 
     getchar();
     return 0;
-}
+}// Fim do programa principal
 
+// Função para verificar se o numero digitado é primo
 int verificaPrimo(int n){
 
     int divisores;
@@ -37,11 +40,17 @@ int verificaPrimo(int n){
             divisores++;
         }
     }
-    
-    if (divisores == 2)
+
+    // Para incluir o numero 1 como um numero primo
+    if (n==1)
     {
         return 1;
-    } else {
+    } 
+    else if (divisores == 2)
+    {
+        return 1;
+    } else 
+    {
         return 0;
     }
 }
