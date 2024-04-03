90c156b29e06b0fc789c58727741667cc3bed398
Alexandre Ribeiro <alexandreribeiro0209@gmail.com>
2024-04-03 19:05:39 -0300

    Atualizacao dos comentarios

--- sistemaBasicoBancario.c
+++ sistemaBasicoBancario.c
@@ -6,6 +6,7 @@
 		- Trocar a senha.
 */
 
+// Importacao de bibliotecas
 #include <stdio.h>
 #include <stdlib.h>
 #include <Windows.h>
@@ -32,8 +33,10 @@ main() {
 	puts("Faca login no sistema");
 	puts("---------------------");
 	
+	// Invocando a funcao de verificacao de Login
 	verLogin();
 	
+	// Invocando a funcao menu
 	menu();
 	
 }// Fim do programa principal
@@ -43,9 +46,11 @@ void verLogin() {
 
 	char usuario = ' ';
 	
+	// Solicita ao usuario o seu login
 	printf("Login: ");
 	scanf(" %c", &usuario);
 	
+	// Transforma o dado recebido em Maiuscula para que não acha diferença entre maiuscula e minuscula. Caso o usuario seja valido invoca a funcao que verifica a Senha, caso seja invalido solicita o login novamente de maneira recursiva
 	if(toupper(usuario) == user) {
 		verSenha();
 	} else {
@@ -59,15 +64,17 @@ void verSenha(){
 
 	int password = 0;
 	
+	// Solicita ao usuario o sua senha
 	printf("Senha: ");
 	scanf("%d", &password);
 	
+	// Se a senha informada for igual a senha do usuario o acesso e liberado caso contrario apos tres tentativas o usuario e bloqueado
 	if (password == senha){
 		cont = 0;
 		puts("Acesso concedido!\n");
 		Sleep(3000);
 	} else {
-		puts("Senha invalida - Tente novamente");
+		puts("Senha invalida - Tente novamente!");
 		cont++;
 		if (cont > 2) {
 			Sleep(3000);
@@ -82,15 +89,20 @@ void verSenha(){
 void menu(){
 
 	int opcaoEscolhida = 0;
+
+	// Imprimir as opcoes de menu
 	puts("+----------------------------------------------------------------------------------------+");
 	puts("| 1 - Consultar Saldo | 2 - Depositar | 3 - Sacar | 4 - Trocar Senha | 5 - Sair da conta |");
 	puts("+----------------------------------------------------------------------------------------+");
 	
+	// Loop Infinito
 	while (1)
 	{
+		// Solicita ao usuario qual opcao do menu ele deseja 
 		printf("\nDigite a opcao desejada: ");
 		scanf("%d", &opcaoEscolhida);
 
+		// Teste logico para invocar a funcao desejada pelo usuario 
 		switch (opcaoEscolhida)
 		{
 			case 1: 
@@ -111,24 +123,33 @@ void menu(){
 				puts("| Sistema finalizado! |");
                 puts("+---------------------+");
 				exit(0);
+			default:
+				puts("Opcao invalida - Tente Novamente!");
 		}
 	}
 }
 
 // Funcao para consultar saldo
 void consultarSaldo() {
+
+	// Imprime o valor do Saldo
 	printf("Saldo: %.2f\n", saldo);
 } // Fim da funcao consultar saldo
 
 // Funcao para depositar
 void depositar(){
+
+	// Solicita a senha novamente ao usuario por seguranca
 	verSenha();
 
 	float deposito = 0.0;
+	
+	// Solicita ao usuario o valor que ele deseja depositar
 	printf("Digite o valor que deseja depositar: ");
 	scanf("%f", &deposito);
 	
-	if(deposito != 0) {
+	// Testa se o valor do deposito e maior que zero antes de permitir o deposito
+	if(deposito > 0) {
 		saldo += deposito;
 		Sleep(3000);
 		puts("Deposito aprovado!");
@@ -139,13 +160,18 @@ void depositar(){
 
 // Funcao para sacar dinheiro
 void sacar(){
+
+	// Solicita a senha novamente ao usuario por seguranca
 	verSenha();
 
 	float saque = 0.0;
+
+	// Solicita ao usuario o valor que ele deseja sacar
 	printf("Digite o valor que deseja sacar: ");
 	scanf("%f", &saque);
 	
-	if(saque != 0) {
+	// Testa se o valor do saque e maior que zero antes de permitir o saque
+	if(saque > 0) {
 		saldo -= saque;
 		Sleep(3000);
 		puts("Saque aprovado!");
@@ -160,15 +186,19 @@ void trocarSenha(){
 	int novaSenha = 0;
 	int novaSenhaAux = 0;
 
+	// Solicita a senha novamente ao usuario por seguranca
 	puts("\nDigite a senha atual");
 	verSenha();
 
+	// Solicita a nova senha ao usuario
 	printf("Digite a nova senha: ");
 	scanf("%d", &novaSenha);
-
+	
+	// Solicita a nova senha ao usuario novamente
 	printf("Digite novamente a nova senha: ");
 	scanf("%d", &novaSenhaAux);
 
+	// Teste logico para verificar se a nova senha foi inserida duas vezes corretamente, caso não tenha invoca a funcao para o usuario tentar novamente
 	if (novaSenha == novaSenhaAux)
 	{
 		Sleep(3000);
